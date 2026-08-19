587591dab83672f0d625774e5178ed52d1c46896
Maciej <maciej.wozniczka@outlook.com>
2026-08-19 18:03:14 +0200

    Added ClickCounter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import ClickCounter from "./components/ClickCounter";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const increaseValue = () => {
-    setCounter(counter + 1);
-  };
   return (
     <>
-      <h1>Witaj!</h1>
-      <p>Kliknięto {counter} razy</p>
-      <button onClick={increaseValue}>Kliknij mnie</button>
+      <ClickCounter />
     </>
   );
 }
