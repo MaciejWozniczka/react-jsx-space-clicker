2f877658de23b4be32604ff3b1256a092ed7d474
Maciej <maciej.wozniczka@outlook.com>
2026-08-21 10:26:02 +0200

    Updated PWA config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     VitePWA({
@@ -15,14 +16,28 @@ export default defineConfig({
         display: "fullscreen",
         icons: [
           {
-            src: "icons/192.png",
+            src: "icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
           },
           {
-            src: "icons/512.png",
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
