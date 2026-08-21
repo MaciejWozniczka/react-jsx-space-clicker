d41c2245d9f9769f4a4c4a5cd26953c5d317edb8
Maciej <maciej.wozniczka@outlook.com>
2026-08-21 10:08:48 +0200

    Added PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Space Clicker",
+        short_name: "Space Clicker",
+        start_url: "./",
+        display: "fullscreen",
+        icons: [
+          {
+            src: "icons/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
